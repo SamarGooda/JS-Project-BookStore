1009481e10fe50b177db391bec925da5440933c5
EsraaSalah28 <es83626@gmail.com>
2020-02-14 18:12:42 +0200

    some modification on description page

--- js/home.js
+++ js/home.js
@@ -25,12 +25,12 @@ todo.addEventListener("click",todoPage)
 category.addEventListener("click",categoriesPage)
 about.addEventListener("click",aboutPage)
 class Book {
-    constructor(name,desc,img,price,page){
+    constructor(name,desc,img,price,author){
         this.name=name;
         this.desc=desc;
         this.img=img;
         this.price=price;
-        this.page=page;
+        this.author=author;
     }
     get bookName (){
          return this.name;
@@ -46,27 +46,27 @@ class Book {
          return this.price;
      }
 
-    //  get bookPage()
-    //  {
-    //      return this.page;
-    //  }
+     get bookAuthor()
+     {
+         return this.author;
+     }
   }
 
-book1 = new Book ("Anna Karena","A beautiful wife from St..","../img/img1.jpg","$19.99")
+book1 = new Book ("Anna Karena","A beautiful wife from St..","../img/img1.jpg","$19.99","Leo Tolstoy")
 booksData.push(book1)
-book2 = new Book ("Sand and Foam","A book of aphorisms, poems, and parables by the author ","../img/book2.jpg","20$")
+book2 = new Book ("Sand and Foam","A book of aphorisms, poems, and parables by the author ","../img/book2.jpg","20$","Gobran Khalel Gobran")
 booksData.push(book2)
-book3 = new Book ("Vineland","The story is set in California, usa","../img/book33.jpg","50$")
+book3 = new Book ("Vineland","The story is set in California, usa","../img/book33.jpg","50$",	"Thomas Pynchon")
 booksData.push(book3)
-book4 = new Book ("	Americana","Ifemelu and Obinze are young and in love ","../img/book4.jpg","22$")
+book4 = new Book ("	Americana","Ifemelu and Obinze are young and in love ","../img/book4.jpg","22$","Chimamanda Ngozi Adichie")
 booksData.push(book4)
-book5 = new Book ("Great Jones Street"," It centers on rock star Bucky Wunderlick,","../img/book5.jpg","23$")
+book5 = new Book ("Great Jones Street"," It centers on rock star Bucky Wunderlick,","../img/book5.jpg","23$","Don DeLillo")
 booksData.push(book5)
-book6 = new Book ("Mason & Dixon"," It presents a fictionalized account of the collaboration ","../img/book6.jpg","25$")
+book6 = new Book ("Mason & Dixon"," It presents a fictionalized account of the collaboration ","../img/book6.jpg","25$","Thomas Pynchon")
 booksData.push(book6)
-book7 = new Book ("Against the Day"," It presents a fictionalized account of the collaboration","../img/book7.jpg","30$")
+book7 = new Book ("Against the Day"," It presents a fictionalized account of the collaboration","../img/book7.jpg","30$","Thomas Pynchon")
 booksData.push(book7)
-book8 = new Book ("Gravity's Rainbow","Lengthy, complex, and featuring a large cast of characters","../img/book8.jpg","10$")
+book8 = new Book ("Gravity's Rainbow","Lengthy, complex, and featuring a large cast of characters","../img/book8.jpg","10$","Thomas Pynchon")
 booksData.push(book8)
 
 function setCardData()
