4e3c561cd1b3579718540a921b579046298b2479
EsraaSalah28 <es83626@gmail.com>
2020-02-14 16:53:37 +0200

    finish stars and making alert with rating

---
+++ js/desc1.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName("btn_btn").click();
+var rating="";
+function starmark(item)
+{ 
+  var count =item.id[0];
+  rating=count;
+  var subid =item.id.substring(1);
+  for(var i=0; i<5;i++)
+  {
+      if(i<count)
+      {
+          document.getElementById((i+1)+subid).style.color="orangered";
+
+      }
+      else
+      {
+        document.getElementById((i+1)+subid).style.color="black";
+ 
+
+      }
+  }
+}
+function showRating()
+{
+    alert(rating);
+}
