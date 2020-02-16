7e25b4be4ac00d94236c3be12ee7ae34cb191a3b
aAbdelnaby88 <ahmed.abdelnapy88@gmail.com>
2020-02-16 20:41:37 +0200

    todo list, change some designs

--- js/home.js
+++ js/home.js
@@ -25,13 +25,14 @@ todo.addEventListener("click",todoPage)
 category.addEventListener("click",categoriesPage)
 about.addEventListener("click",aboutPage)
 class Book {
-    constructor(id,name,desc,img,price,author){
+    constructor(id,name,desc,img,price,author,flag){
         this.id=id;
         this.name=name;
         this.desc=desc;
         this.img=img;
         this.price=price;
         this.author=author;
+        this.flag=flag
     }
     get bookId()
     {
@@ -55,23 +56,27 @@ class Book {
      {
          return this.author;
      }
+     get bookFlag()
+     {
+         return this.flag;
+     }
   }
 
-book1 = new Book ("1","Anna Karena","A beautiful wife from St..","../img/img1.jpg","$19.99","Leo Tolstoy")
+book1 = new Book ("1","Anna Karena","A beautiful wife from St..","../img/img1.jpg","$19.99","Leo Tolstoy","0")
 booksData.push(book1)
-book2 = new Book ("2","Sand and Foam","A book of aphorisms, poems, and parables by the author ","../img/book2.jpg","20$","Gobran Khalel Gobran")
+book2 = new Book ("2","Sand and Foam","A book of aphorisms, poems, and parables by the author ","../img/book2.jpg","20$","Gobran Khalel Gobran","0")
 booksData.push(book2)
-book3 = new Book ("3","Vineland","The story is set in California, usa","../img/book33.jpg","50$",	"Thomas Pynchon")
+book3 = new Book ("3","Vineland","The story is set in California, usa","../img/book33.jpg","50$",	"Thomas Pynchon","0")
 booksData.push(book3)
-book4 = new Book ("4","	Americana","Ifemelu and Obinze are young and in love ","../img/book4.jpg","22$","Chimamanda Ngozi Adichie")
+book4 = new Book ("4","	Americana","Ifemelu and Obinze are young and in love ","../img/book4.jpg","22$","Chimamanda Ngozi Adichie","0")
 booksData.push(book4)
-book5 = new Book ("5","Great Jones Street"," It centers on rock star Bucky Wunderlick,","../img/book5.jpg","23$","Don DeLillo")
+book5 = new Book ("5","Great Jones Street"," It centers on rock star Bucky Wunderlick,","../img/book5.jpg","23$","Don DeLillo","0")
 booksData.push(book5)
-book6 = new Book ("6","Mason & Dixon"," It presents a fictionalized account of the collaboration ","../img/book6.jpg","25$","Thomas Pynchon")
+book6 = new Book ("6","Mason & Dixon"," It presents a fictionalized account of the collaboration ","../img/book6.jpg","25$","Thomas Pynchon","0")
 booksData.push(book6)
-book7 = new Book ("7","Against the Day"," It presents a fictionalized account of the collaboration","../img/book7.jpg","30$","Thomas Pynchon")
+book7 = new Book ("7","Against the Day"," It presents a fictionalized account of the collaboration","../img/book7.jpg","30$","Thomas Pynchon","0")
 booksData.push(book7)
-book8 = new Book ("8","Gravity's Rainbow","Lengthy, complex, and featuring a large cast of characters","../img/book8.jpg","10$","Thomas Pynchon")
+book8 = new Book ("8","Gravity's Rainbow","Lengthy, complex, and featuring a large cast of characters","../img/book8.jpg","10$","Thomas Pynchon","0")
 booksData.push(book8)
 
 function setCardData()
--- js/todo.js
+++ js/todo.js
@@ -22,25 +22,105 @@ todo.addEventListener("click",todoPage)
 category.addEventListener("click",categoriesPage)
 about.addEventListener("click",aboutPage)
 
-var arr=JSON.parse(localStorage.getItem("booksData"));
 
+var arr=JSON.parse(localStorage.getItem("booksData"));
+var username= JSON.parse(localStorage.getItem("loginUser"));
+var usersObject=JSON.parse(localStorage.getItem("users"))
 var reading= document.getElementsByClassName("reading")[0];
 var finished= document.getElementsByClassName("finished")[0];
 
-arr.forEach((card)=>{
+function getUser()
+{
+    for(var i=0;i<usersObject.length;i++)
+    {
+        if(usersObject[i].name==username)
+        {
+            setReadingList(usersObject[i].read)
+            setFinishedList(usersObject[i].fRead)
+        }
+    }
+}
+getUser();
+
+function setReadingList(x)
+{
+    for(let i=0;i<x.length;i++)
+    {
     const ca= document.createElement('div');
     ca.classList='container'
 
     const content=`<div class ="column">
                     <div class="card">
-        <img class ="bimg"src="${card.img}">
+        <img class ="bimg"src="${x[i].img}">
         <div class="container">
-          <h4 class="title"><b>${card.name}</b></h4>
-          <p class="author">${card.desc}</p>
+          <h4 class="title"><b>${x[i].name}</b></h4>
+          <button class="author" onclick="setFinished(${x[i].id})">Finished</button>
         </div>
       </div>
     </div>`
     reading.innerHTML += content;
+        }
+}
 
+function setFinishedList(y)
+{
+    for(let i=0;i<y.length;i++)
+    {
+    const ca= document.createElement('div');
+    ca.classList='container'
+
+    const content=`<div class ="column">
+                    <div class="card">
+        <img class ="bimg"src="${y[i].img}">
+        <div class="container">
+          <h4 class="title"><b>${y[i].name}</b></h4>
+          <button class="author" onclick="removeCard(${y[i].id})">Remove</button>
+        </div>
+      </div>
+    </div>`
     finished.innerHTML += content;
-})
+        }
+}
+
+function setFinished(z)
+{
+    for(var i=0;i<usersObject.length;i++)
+    {
+        if(usersObject[i].name==username)
+        {
+            for(let j=0;j<usersObject[i].read.length;j++)
+            {
+                if(usersObject[i].read[j].id==z)
+                {
+                    usersObject[i].fRead.push(usersObject[i].read[j])
+                    usersObject[i].read.splice(j,1)
+                    localStorage.setItem("users", JSON.stringify(usersObject))
+                    getUser();
+                    alert("Congrats you have Finished this Book ^_^!!")
+                    location.reload();
+                }
+            }
+        }
+    }
+}
+
+function removeCard(v)
+{
+ for(var i=0;i<usersObject.length;i++)
+    {
+        if(usersObject[i].name==username)
+        {
+            for(let j=0;j<usersObject[i].fRead.length;j++)
+            {
+                if(usersObject[i].fRead[j].id == v)
+                {
+                    usersObject[i].fRead.splice(j,1)
+                    localStorage.setItem("users", JSON.stringify(usersObject))
+                    getUser();
+
+                    location.reload();
+                }
+            }
+        }
+    }
+}
